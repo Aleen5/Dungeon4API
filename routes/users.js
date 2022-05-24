cb8a810a277344d13a6e977ad2d63a9416625964
Alex <rodriguezespinosaa@gmail.com>
2022-05-24 13:20:07 +0200

    Añadido temporalmente get de User (falla de seguridad gorda, ojo)

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var users = require("../controllers/users");
 const bodyParser = require('body-parser').json()
 
 //router.get("/users", users.list);
-//router.get("/users/:username", users.get);
+router.get("/users/:username", users.get);
 router.get("/users/:username/:password", users.log); // LOGIN
 router.post("/users", bodyParser, users.add);
 router.put("/users/:username", bodyParser, users.update);
